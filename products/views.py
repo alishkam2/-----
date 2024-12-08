6361fa6c36bc9de38ea441ff4c0e3e1d9ead473a
alishka-m <alimrahimov508@gmail.com>
2024-12-08 17:05:02 +0600

    урок2

--- products/views.py
+++ products/views.py
@@ -5,22 +5,22 @@ from .models import Product
 from .serializers import ProductSerializer, ProductDetailSerializer
 
 
-@api_view(["GET"])
+@api_view(['GET'])
 def product_detail_api_view(request, id):
     try:
         product = Product.objects.get(id=id)
     except Product.DoesNotExist:
-        return Response(
-            data={"error": "Product not found!"}, status=status.HTTP_404_NOT_FOUND
-        )
+        return Response(data={'error': 'Product not found!'},
+                        status=status.HTTP_404_NOT_FOUND)
     data = ProductDetailSerializer(product).data
     return Response(data=data)
 
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=['GET'])
 def product_list_api_view(request):
     # step 1: Collect all products from DB (QuerySet)
-    products = Product.objects.all()
+    products = (Product.objects.select_related('category')
+                .prefetch_related('tags', 'reviews').all())
 
     # step 2: Reformat (Serialize) data to list of Dictionary
     serializer = ProductSerializer(instance=products, many=True)
